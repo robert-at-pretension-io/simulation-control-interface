836df2eddeb398f2ad28cd7e86cc2e64a88051f6
robert-at-pretension-io <robert@pretension.io>
2020-09-27 10:45:56 -0400

    added a working websocket server that can send and receive data

--- websocket_server/src/main.rs
+++ websocket_server/src/main.rs
@@ -1,69 +1,79 @@
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::stream::StreamExt;
 use tokio::sync::{broadcast, mpsc, oneshot};
-use tokio::prelude::*;
 
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
+use futures_util::SinkExt;
 
-//use std::io;
 use tokio_tungstenite;
 
+use std::net::IpAddr;
+
+enum ConnectionStatus {
+    Offline,
+    Online,
+    WithPartner,
+    WaitingForPartner
+}
+
+
+impl Default for ConnectionStatus {
+    fn default() -> Self {
+        ConnectionStatus::Online
+    }
+}
+
+struct Connection {
+    connection_status : ConnectionStatus,
+    ip_address : IpAddr
+}
+
+impl Connection {
+    fn new(ip_address : IpAddr, connection_status : Option<ConnectionStatus>) -> Self {
+        Connection {
+            connection_status : connection_status.unwrap_or(ConnectionStatus::default()),
+            ip_address
+        }
+    }
+}
 
 #[tokio::main]
 async fn main()  {
     let mut listener = TcpListener::bind("127.0.0.1:80").await.unwrap();
 
-    //let mut WebSocketStreamVector = Vec::<tokio_tungstenite::WebSocketStream<S>>::new();
+
+    let shared_connections = Arc::new(Mutex::new(Vec::<Connection>::new()));
+
+    let (mut new_connection_channel_tx,mut new_connection_channel_rx) = mpsc::channel::<ConnectionStatus>::(10);
 
     tokio::spawn(async move {
 
         while let Some(stream) = listener.next().await {
             match stream {
-                Ok(stream) => {
+                Err(e) => {println!("{:?}", e)}
+                Ok(mut stream) => {
+
+                    
+
                     println!("new client! Let's try upgradding them to a websocket connection on port 80!");
 
                     
 
-                    match tokio_tungstenite::accept_async(stream).await {
-                        Err(e) => {println!("sad times... an error occurred:\n {:?}", e);}
-                        Ok(mut websocket_stream) => {
-                            println!("Seems it ACTUALLY worked... now just to do something with the stream");
+                    let mut ws_stream = tokio_tungstenite::accept_async(stream).await.expect("failed to accept websocket.");
+
+                    let message =  tungstenite::Message::text(String::from("hello from the server!"));
                             
+                    ws_stream.send(message).await.unwrap();
 
-                            let address = websocket_stream.get_ref().peer_addr().unwrap();
-                            
-                            println!("Peer at address {} has connected.", address);
+                    let address = ws_stream.get_ref().peer_addr().unwrap();
 
-
-                            match websocket_stream.get_mut().write(b"Test.").await {
-                                Ok(data_size) => {println!("supposedly just wrote this much data: {}!", data_size);},
-                                Err(uhhh) => {println!("oh man, look at that error... wild: {}", uhhh)}
-                            }
-                                
-                            
-                        
-
-                        while let Some(stuff) = websocket_stream.try_next().await.unwrap() {
-                            println!("The server says: ooooo boy, someone to talk to! The person at {} said {}", address, stuff);
-
-
-
-
-                            
-
-                            // println!("Of course, this simply wouldn't do so I disconnected promptly. The fucker.");
-                            // websocket_stream.close(None).await;
-                        }
-
-
-                        }
+                    while let Some(stuff) = ws_stream.try_next().await.unwrap() {
+                        println!("The server says: ooooo boy, someone to talk to! The person at {} said {}", address, stuff);
                     }
-    
-    
                 }
-                Err(e) => { /* connection failed */ }
             }
         }
-
     }).await.unwrap();
 }
