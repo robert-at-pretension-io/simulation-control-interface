15881167d5366e603c22d33dd7579d9473296cbf
robert-at-pretension-io <robert@pretension.io>
2020-10-12 19:48:09 -0400

    starting to add states to the frontend client

--- websocket_server/src/main.rs
+++ websocket_server/src/main.rs
@@ -2,6 +2,7 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::stream::StreamExt;
 use tokio::sync::mpsc;
 
+
 use futures_util::SinkExt;
 
 use tokio_tungstenite;
@@ -12,11 +13,9 @@ use std::collections::HashMap;
 use std::net::SocketAddr;
 
 use tracing::{instrument, Level};
-use tracing_subscriber::{fmt::format, fmt::format::FmtSpan, fmt};
-use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt::format::FmtSpan};
 use log::info;
 
-use tokio::sync::oneshot;
 
 #[derive(Debug)]
 enum ConnectionStatus {
@@ -37,19 +36,19 @@ impl Default for ConnectionStatus {
 struct Connection {
     connection_status: ConnectionStatus,
     ip_address: SocketAddr,
-    // ws_oneshot_transmitter: Option<mpsc::Sender<String>>,
+    ws_mpsc_transmitter: Option<mpsc::Sender<String>>,
 }
 
 impl Connection {
     fn new(
         ip_address: SocketAddr,
         connection_status: Option<ConnectionStatus>,
-        // ws_oneshot_transmitter: Option<mpsc::Sender<String>>,
+        ws_mpsc_transmitter: Option<mpsc::Sender<String>>,
     ) -> Self {
         Connection {
             connection_status: connection_status.unwrap_or(ConnectionStatus::default()),
             ip_address,
-            // ws_oneshot_transmitter,
+            ws_mpsc_transmitter,
         }
     }
 }
@@ -57,17 +56,19 @@ impl Connection {
 #[instrument]
 async fn ws_connection(
     mut tx_status_manager: mpsc::Sender<Connection>,
-    mut rx_ws_watch : watch::Receiver<String>,
+     process_complete_channel_tx_rx : (mpsc::Sender<String>,mpsc::Receiver<String>),
     stream: TcpStream,
     //ws_transmitter: mpsc::Sender<String>,
 ) {
 
+    let (tx, mut rx) = process_complete_channel_tx_rx;
+
     let ip_address = stream.peer_addr().unwrap();
 
     tx_status_manager
-        .send(Connection::new(ip_address, None))
+        .send(Connection::new(ip_address, None,Some(tx)))
         .await
-        .expect("The connection was closed :[");
+        .expect("The connection was closed before even getting to update the status within a system. The odds of this happening normally are extremely low... Like someone would have to connection and then almost instantaneously close the connection:[");
 
     info!("new client! Let's try upgradding them to a websocket connection on port 80!");
 
@@ -83,26 +84,43 @@ async fn ws_connection(
 
     let address = ws_stream.get_ref().peer_addr().unwrap();
 
-    while let Some(stuff) = ws_stream.try_next().await.unwrap() {
-        if stuff.is_close() {
+
+    loop { 
+        tokio::select! {
+
+            val = rx.next() => {
+            if val.is_some() {
+
+                info!("looky! {:?}... \n This indicates that the status manager is able to comunicate back to the client through the websocket!", &val);
+
+                let message = tungstenite::Message::text(String::from("The server acknowledges that you're online!"));     
+                
+                ws_stream.send(message).await.un
+            
+            }
+            },
+            val = ws_stream.try_next() => {
+                let val = val.unwrap();
+                if val.is_some() { info!("{:?}", val); }
+        if val.unwrap().is_close() {
             info!("The client is trying to close the connection");
 
             tx_status_manager
                 .send(Connection::new(
                     ip_address,
                     Some(ConnectionStatus::ClosedConnection),
-                    
+                    None
                 ))
                 .await
                 .expect("The connection was closed :[");
             break;
         }
-        info!(
-            "The server says: ooooo boy, someone to talk to! The person at {} said \"{}\"",
-            address,
-            stuff.to_string()
-        );
+
+            },
+        };
     }
+
+
 }
 
 #[instrument]
@@ -111,13 +129,15 @@ async fn status_manager(mut status_updater_rx :  tokio::sync::mpsc::Receiver<Con
     let mut connection_status = HashMap::<SocketAddr, ConnectionStatus>::new();
 
     while let Some(connection) = status_updater_rx.recv().await {
+
+        info!("Received connection in status_manager");
+
         match connection.connection_status {
             ConnectionStatus::Online => {
-                // let mut ws_oneshot_transmitter = connection
-                //     .ws_oneshot_transmitter
-                //     .expect("The connection must go through!");
 
-                // ws_oneshot_transmitter
+                connection.ws_mpsc_transmitter.unwrap().send(String::from("sweet sweet acknowledgement!")).await;
+
+                // ws_mpsc_transmitter
                 //     .send(String::from("Added the connection"))
                 //     .await
                 //     .expect("fugck, couldn't send the message");
@@ -142,22 +162,14 @@ async fn status_manager(mut status_updater_rx :  tokio::sync::mpsc::Receiver<Con
 #[tokio::main]
 async fn main() {
 
-    //let formatter = format::debug_fn(|writer, field, value| write!(writer, "{} : {:?}", field, value)).delimited(",\n");
-
-    //read more about debug_fmt here: https://docs.rs/tracing-subscriber/0.2.12/tracing_subscriber/fmt/struct.SubscriberBuilder.html
-
     tracing_subscriber::fmt()
     .with_max_level(Level::DEBUG)
     .compact()
-    //.fmt_fields(formatter)
-    //.event_format() //Still need to figure this one out... Seems like a way to format specific events... Could be useful later on when the project evolves.
     .with_level(true)
-    .with_target(true)
-    .with_span_events(FmtSpan::FULL)
-    // sets this to be the default, global subscriber for this application.
+    .with_target(false)
+    //.with_span_events(FmtSpan::FULL)
     .init();
     
-    //tracing_subscriber::fmt::Sub
     
 
     let mut listener = TcpListener::bind("127.0.0.1:80").await.unwrap();
@@ -166,28 +178,29 @@ async fn main() {
     
     let (status_updater_tx, status_updater_rx) = mpsc::channel::<Connection>(10);
     
-
+    // Connection status manager, user/connection states are updated here but no "functionality" is really performed.
+    tokio::spawn(async {
+        info!("setting up a status manager");
+        status_manager(status_updater_rx).await
+    });
 
     // websocket manager
     while let Some(Ok(stream)) = listener.next().await {
         // let mut tx_clone = tx.clone();
         let status_updater_tx_clone = status_updater_tx.clone();
     
-        let process_complete_channel_tx_rx = oneshot::channel();
+        let process_complete_channel_tx_rx = mpsc::channel(10);
 
         // this is a point in the program where the channel should be split up:
         // Both the tx and rx should will be sent to ws_connection. The tx part will then be sent to the status updater as a message
 
-        tokio::spawn(async  { ws_connection(status_updater_tx_clone, ws_rx_clone ,stream).await });
+        tokio::spawn(async  { ws_connection(status_updater_tx_clone, process_complete_channel_tx_rx ,stream).await });
     }
 
-    // Connection status manager, user/connection states are updated here but no "functionality" is really performed.
-    tokio::spawn(async {
-        status_manager(status_updater_rx).await
-    });
+
 
     // ----------   Process handler   ----------
-    // This needs two oneshot channels.
+    // This needs two mpsc channels.
     // 1. status manager -> process handler
     // 2. process handler -> websocket manager
     //
