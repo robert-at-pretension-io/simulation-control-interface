ce9c80d75012e0272cbaf97f6f362f4ecde98a2f
robert-at-pretension-io <robert@pretension.io>
2020-11-18 17:02:02 -0500

    ok... now to delete half of these commits :/

--- yew-frontend/src/lib.rs
+++ yew-frontend/src/lib.rs
@@ -1,33 +1,31 @@
-use wasm_bindgen::prelude::*;
-use yew::prelude::*;
-use web_sys::{MessageEvent, WebSocket};
-use wasm_bindgen::JsCast;
 use js_sys;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{MessageEvent, WebSocket};
+use yew::prelude::*;
 
 use models::ControlMessages;
 
-static WEBSOCKET_URL : &str = "ws://127.0.0.1:80";
+static WEBSOCKET_URL: &str = "ws://127.0.0.1:80";
 
 struct Model {
     event_log: Vec<String>,
-    current_state: State,
     user: String,
     link: ComponentLink<Self>,
     websocket: Option<WebSocket>,
 }
 
-enum State {
-    WelcomeScreen,
-    ConnectedToWebsocketServer,
-}
 
 enum Msg {
-    ConnectToServer,
+    InitialPage,
+    InitiateWebsocketConnectionProcess,
+    EstablishingConnectionToWebsocketServer(u8),
+    ConnectedToWebsocketServer,
     ReadyForPartner,
     UpdateUsername(String),
+    LogEvent(String),
     ServerSentWsMessage(String),
 }
-
 
 extern crate web_sys;
 
@@ -35,14 +33,13 @@ impl Model {
     fn show_events_in_table(&self) -> Html {
         html!(
             <>
-            {for self.event_log.iter().map(|event| { 
+            {for self.event_log.iter().map(|event| {
                 html!(<div> <p> {event} </p> </div>)
             })  }
             </>
         )
     }
 }
-
 
 impl Component for Model {
     type Message = Msg;
@@ -53,75 +50,118 @@ impl Component for Model {
             link,
             websocket: None,
             event_log: Vec::<String>::new(),
-            current_state: State::WelcomeScreen,
             user: String::from("Random User"),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
+            Msg::InitialPage => {
+                true
+            }
+            Msg::LogEvent(event) => {
+                self.event_log.push(event);
+                true
+            }
             Msg::ServerSentWsMessage(message) => {
-                self.event_log.push(message);
-                true
-            },
-            Msg::ConnectToServer => {
+                self.link.send_message(Msg::LogEvent(message));
+                false
+            }
+            Msg::InitiateWebsocketConnectionProcess => {
+                let ws = WebSocket::new(WEBSOCKET_URL).unwrap();
 
-                let ws =  WebSocket::new(WEBSOCKET_URL).unwrap();
-                //TODO: setup the websocket connection here...
-                // * add listener and stuff?
-                // * the listener should send messages to YEW
-                
-                let cloned = self.link.clone();
+                if ws.ready_state() == 1 {
+                    self.websocket = Some(ws);
 
-                // The onmessage_callback handles MOOOOST of the logic of client-server communication
-                // This callback is suuuper important lol
-                let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
-                    if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
-                            //web_sys::console::log_1(&txt);
+                    let messages : Vec<Msg> = vec!(
+                        Msg::LogEvent("The connection is open and ready to communicate.".to_string()),
+                        Msg::ConnectedToWebsocketServer
+                    );
+                    self.link.send_message_batch(messages);
+                    false
+                }
+                else {
 
-                            cloned.send_message(Msg::ServerSentWsMessage(txt.into()))
-                    } 
-
-                    else if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
-                        //console_log!("message event, received arraybuffer: {:?}", abuf);
-                        let array = js_sys::Uint8Array::new(&abuf);
-                        //let len = array.byte_length() as usize;
-                        //console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
-
-
-                        web_sys::console::log_1(&abuf.to_string());
-                        
-
-                        match ControlMessages::deserialize(&array.to_vec()) {
-                            ControlMessages::Id(new_id) => {
-                                //update the clients id
-                            }
-
-                            ControlMessages::Message(message) => {
-                                cloned.send_message(Msg::ServerSentWsMessage(message.into()))
-                            }
-
-                            ControlMessages::ServerInitiated => {
-                                cloned.send_message(Msg::ServerSentWsMessage(String::from("Oh.. I guess the server said hi! ... Wow. I'm so embarassed!")))
-                            }
-
-                        }
-
-
-                    }
-                }) as Box<dyn FnMut(MessageEvent)>);
-                // set message event handler on WebSocket
-                ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
-                // forget the callback to keep it alive
-                onmessage_callback.forget();
-                
                 self.websocket = Some(ws);
-                self.current_state = State::ConnectedToWebsocketServer;
-                true
+                let messages : Vec::<Msg> = vec!(
+                    Msg::LogEvent("attempting ws connection ...".to_string()),
+                    Msg::EstablishingConnectionToWebsocketServer(1),
+            ); 
+                self.link.send_message_batch(messages);
+                false
+                }
+
+
+
             }
             Msg::ReadyForPartner => false,
             Msg::UpdateUsername(username) => {
                 self.user = username;
+                true
+            }
+            Msg::EstablishingConnectionToWebsocketServer(attempt) => {
+                if attempt > 3 {
+                    let messages = vec![
+                        Msg::LogEvent("Ran out of connection attempts...".to_string()),
+                        Msg::InitialPage
+                    ];
+
+                    self.link.send_message_batch(messages);
+                    true
+                }
+                else {
+                let state = match &self.websocket {
+                    Some(ws) => {
+                        
+                        ws.ready_state()
+                    },
+                    None => {
+                        let messages : Vec<Msg> = vec!(
+                            Msg::LogEvent("Somehow there is no websocket...".to_string()),
+                            Msg::InitiateWebsocketConnectionProcess
+                        );
+                        self.link.send_message_batch(messages);
+                        3 // we'll just show an error because the websocket wasn't 
+                    }
+                };
+
+                self.link.send_message(Msg::LogEvent(format!("Current State: {}", state)));
+
+                if state == WebSocket::OPEN  {
+                    let messages : Vec<Msg> = vec!(
+                        Msg::LogEvent("The connection is open and ready to communicate.".to_string()),
+                        Msg::ConnectedToWebsocketServer
+                    );
+                    self.link.send_message_batch(messages);
+                   } 
+
+               else if state == WebSocket::CONNECTING  {
+                   
+                let messages : Vec<Msg> = vec!(
+                    Msg::LogEvent("Socket has been created. The connection is not yet open.".to_string()),
+                    Msg::EstablishingConnectionToWebsocketServer(attempt + 1)
+                );
+                self.link.send_message_batch(messages);
+               } 
+               
+               else if state == WebSocket::CLOSED{
+                let messages : Vec<Msg> = vec!(
+                    Msg::LogEvent("The connection is closed or couldn't be opened.".to_string()),
+                    Msg::InitiateWebsocketConnectionProcess
+                );
+                self.link.send_message_batch(messages);
+               }
+                // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/readyState
+                // ws.readyState is an enum with the values...
+//                  Value 	State 	    Description
+//                  0 	    CONNECTING 	Socket has been created. The connection is not yet open.
+//                  1 	    OPEN 	    The connection is open and ready to communicate.
+//                  2 	    CLOSING 	The connection is in the process of closing.
+//                  3 	    CLOSED 	    The connection is closed or couldn't be opened.
+true
+}
+            }
+            Msg::ConnectedToWebsocketServer => {
                 true
             }
         }
@@ -132,16 +172,19 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
-        match self.current_state {
-            State::WelcomeScreen => {
                 html! {
                     <div>
                         <h1>
                         {"Welcome!"}
                         </h1>
 
+                        <div>
+                        <p> {format!("The following details the event log of the application:")} </p>
+                        {self.show_events_in_table() }
+                        </div>
+
                         <button onclick=self.link.callback(|_| {
-                            Msg::ConnectToServer
+                            Msg::InitiateWebsocketConnectionProcess
                         })>
                             {"Click here to connect to the server."}
                         </button>
@@ -150,25 +193,9 @@ impl Component for Model {
 
                     </div>
                 }
-            }
-            State::ConnectedToWebsocketServer => {
-                
-                // TODO: display the websocket events here!
-                html! {
-                    <div>
-                        <h1> {"You're connected to the server!"} </h1>
-                        {if false {html!{<p> {"Cool"} </p>}} else {html!{<></>}}   }
-                        <p> {format!("Here are some details about the connection:\n {:?}", self.websocket)} </p>
-                        {self.show_events_in_table() }
-                    </div>
 
-                    
-
-                }
-            }
         }
     }
-}
 
 
 
