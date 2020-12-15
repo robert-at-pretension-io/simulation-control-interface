0cdfd1472de48d8109b18513cdd3455bf54fa51c
robert-at-pretension-io <robert@pretension.io>
2020-12-15 18:37:31 -0500

    haha nope

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -5,7 +5,7 @@ use std::collections::{HashMap, HashSet};
 use std::net::SocketAddr;
 
 
-
+// flip... too much going on today. Well I sorta just want to sleep :/
 
 #[derive(Debug, Serialize,Deserialize, Eq, Hash, Clone)]
 pub struct Client {
